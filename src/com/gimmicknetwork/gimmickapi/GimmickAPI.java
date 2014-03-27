f2d19ef4216d08144215db2ac96bf05cdb6fb63e
TeaJizzle <tom@tomjervis.co.uk>
2014-03-27 06:16:22 +0000

    Initial commit

---
+++ src/com/gimmicknetwork/gimmickapi/GimmickAPI.java
@@ -0,0 +1,10 @@
+package com.gimmicknetwork.gimmickapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class GimmickAPI extends JavaPlugin {
+	
+	public void onEnable() { }
+	
+	public void onDisable() { }
+}
