6b11d2fc5c620e9ef4fe67f4132b146e00803bbd
TeaJizzle <tom@tomjervis.co.uk>
2014-03-27 06:23:20 +0000

    implement pvp modes for players with static getter/setter

--- src/com/gimmicknetwork/gimmickapi/GimmickAPI.java
+++ src/com/gimmicknetwork/gimmickapi/GimmickAPI.java
@@ -1,10 +1,31 @@
 package com.gimmicknetwork.gimmickapi;
 
+import java.util.HashMap;
+
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class GimmickAPI extends JavaPlugin {
 	
-	public void onEnable() { }
+	private static HashMap<Player, String> pvpModes = new HashMap<Player, String>();
+	private static String defaultPvpMode = "vanilla";
+	
+	public void onEnable() {
+		this.saveDefaultConfig();
+		defaultPvpMode = this.getConfig().getString("pvp.default", "vanilla");
+	}
 	
 	public void onDisable() { }
+	
+	public static void setPvpModeForPlayer(Player player, String pvpMode) {
+		pvpModes.put(player, pvpMode);
+	}
+	
+	public static String getPvpModeForPlayer(Player player) {
+		if (pvpModes.containsKey(player)) {
+			return pvpModes.get(player);
+		} else {
+			return defaultPvpMode;
+		}
+	}
 }
